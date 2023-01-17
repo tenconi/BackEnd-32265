67227c6807baeb55c839ce848bdc38db88c0b47e
tenconi <47565305+tenconi@users.noreply.github.com>
2023-01-17 09:21:58 +0100

    avance en generateID

--- ProductManager.js
+++ ProductManager.js
@@ -7,28 +7,29 @@ class ProductManager {
     this.path = "./Archivo.JSON";
   }
 
-  async crearProducto(prod) {
-    const {title, description, price, thumbnail, code, stock} = prod;
+  async addProduct(prod) {
+    const { title, description, price, thumbnail, code, stock } = prod;
     const producto = {
-        id: this.generarID(),
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
-    }
+      id: this.generateID(),
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
 
-    const consultoFile = await this.consultarArchivo();
-    consultoFile.push( producto);
-    await fs.promises.writeFile(this.path, JSON.stringify(consultoFile));    
+    const consultoFile = await this.getFile();
+    consultoFile.push(producto);
+    await fs.promises.writeFile(this.path, JSON.stringify(consultoFile));
   }
 
-  async consultarArchivo(prod) {
+  async getFile() {
     try {
       if (fs.existsSync(this.path)) {
         const productos = await fs.promises.readFile(this.path, "utf-8");
         const productosJS = JSON.parse(productos);
+        // return console.log(productosJS[1].id);
         return productosJS;
       } else {
         return [];
@@ -38,35 +39,22 @@ class ProductManager {
     }
   }
 
-    generarID(){
-        let id = 1;
-    
-        try {
-            let estado =  this.consultarArchivo(); // {objeto}
+  async generateID() {
+    try {
+      let estado = await this.getFile();
+      let id = 1;
 
-        // let estadoParse = JSON.parse(estado)
-        let estadoStringify =  JSON.stringify(estado) // solo lee un "objeto_vacio"
-        return estadoStringify
-
-        } catch (error) {
-            console.log(error);
-        }
-        
-
-       
-        
-        /* if(!estadoJS.length){
-            id = estadoJS[estadoJS.length - 1].id + 1;
-        }  */
-        // return id 
-        
+      if (estado != 0) {
+        id = estado[estado.length - 1].id + 1;
+      }
+      return id; // devuelve correctamente
+    } catch (error) {
+      console.log(" → → → id ERROR!", error);
     }
-
-    
+  }
 }
 
-/* TEST */
-
+/* ↓ ↓ ↓ TEST ↓ ↓ ↓ */
 const producto = new ProductManager();
 
 const objeto1 = {
@@ -88,31 +76,26 @@ const objeto2 = {
   stock: 7,
 };
 const objeto3 = {
-    title: "Producto Tres",
-    description: "Esto es un producto tricotero.",
-    price: 30,
-    thumbnail:
-      "https://st3.depositphotos.com/1654249/13060/i/450/depositphotos_130607128-stock-photo-3d-gold-number-one-isolated.jpg",
-    code: "code003",
-    stock: 4,
-  };
+  title: "Producto Tres",
+  description: "Esto es un producto tricotero.",
+  price: 30,
+  thumbnail:
+    "https://st3.depositphotos.com/1654249/13060/i/450/depositphotos_130607128-stock-photo-3d-gold-number-one-isolated.jpg",
+  code: "code003",
+  stock: 4,
+};
 
 /* ---- */
 async function testeando() {
-//   const consultaArchivo = await producto.consultarArchivo();
-//   console.log(consultaArchivo);
+  // const consultaArchivo = await producto.getFile();
+  // console.log(consultaArchivo);
 
-// # Creo Productos
-//   await producto.crearProducto(objeto1);
-//   await producto.crearProducto(objeto2);
-//   await producto.crearProducto(objeto3);
+  // # Creo Productos
+  await producto.addProduct(objeto1);
+  await producto.addProduct(objeto2);
+  await producto.addProduct(objeto3);
 
-console.log(producto.generarID())
+  // # producto.generateID();
+  // console.log(await producto.generateID());
 }
 testeando();
-
-// • Devuelvo productos:
-// console.log(prod1.devolverProductos());
-
-// • Busco producto por Id:
-// prod1.obtenerPorID(2)
