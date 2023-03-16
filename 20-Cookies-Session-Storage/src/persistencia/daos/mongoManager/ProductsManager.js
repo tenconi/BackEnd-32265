43ed02aa2b99ed396284d04b14f104de354d448e
tenconi <47565305+tenconi@users.noreply.github.com>
2023-03-16 09:32:12 +0100

    ajuste HBS en productos

--- 20-Cookies-Session-Storage/src/persistencia/daos/mongoManager/ProductsManager.js
+++ 20-Cookies-Session-Storage/src/persistencia/daos/mongoManager/ProductsManager.js
@@ -3,12 +3,21 @@ import { productsModel } from '../../models/products.models.js';
 export default class ProductsManager {
 
     async getAllProducts(limit, page) {
-
         try {
             const getProds = await productsModel.aggregatePaginate({}, { limit, page });
             // const getProds = await productsModel.find( {});
             // console.log(getProds.docs);
             return getProds;
+        } catch (error) {
+            return error
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const getProdById = await productsModel.find({'_id' : id});
+            // console.log(getProdById);
+            return getProdById
         } catch (error) {
             return error
         }
--- 20-Cookies-Session-Storage/src/routes/products.router.js
+++ 20-Cookies-Session-Storage/src/routes/products.router.js
@@ -3,16 +3,28 @@ import ProductsManager from '../persistencia/daos/mongoManager/ProductsManager.j
 
 const router = Router();
 
-const productManger = new ProductsManager(); // instancio
+const productManager = new ProductsManager(); // instancio
 
-router.get('/todos', async (req, res) => {
+router.get('/todos', async (req, res) => { // → si dejo solo el "/" No levanta la pag.
     const { limit = 10, page = 1 } = req.query; // x ej:  http://localhost:8080/products?limit=2&page=1
-    const products = await productManger.getAllProducts(limit, page);
+    const products = await productManager.getAllProducts(limit, page);
     // console.log('products', products);
     if (products.length === 0) {
-        res.send({ message: 'No hay productos listados' });
+        res.send('No hay productos listados');
     } else {
-        res.render('products' , {products , limit , page })
+        res.render('products', { products, limit, page })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const singleProduct = await productManager.getProductById( id );
+    
+    if (singleProduct) {
+        res.render('products', {singleProduct} );
+        // console.log(singleProduct);
+    } else {
+        res.send(`Lamentablemente el Id ${id} no se encuentra listado.`);
     }
 })
 
