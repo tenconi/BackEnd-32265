e36356475af6a797977c95e4fd4da02c27f89792
tenconi <47565305+tenconi@users.noreply.github.com>
2023-03-21 13:55:48 +0100

    ea

--- 22- Estrategia de autenticación por terceros + JWT/src/persistencia/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-try {
-    mongoose.connect('mongodb+srv://tenco:Bonetaso16@cluster0.5xmnrmy.mongodb.net/ecommerce?retryWrites=true&w=majority');
-    console.log('* * * Conectado a Base de Datos');
-} catch (error) {
-    console.log(error);
-}
--- 22- Estrategia de autenticación por terceros + JWT/src/routes/chat.router.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get( '/' , (req, res) => {
-    res.json({message: ' chat'})
-})
-
-export default router
