de506edf5a749bdf970f3b92c8550132bc9bf3d6
tenconi <47565305+tenconi@users.noreply.github.com>
2023-03-21 10:08:20 +0100

    clase 20: cookies, session & storage :hear_no_evil:

--- 20-Cookies-Session-Storage/src/persistencia/middlewares/userRol.js
+++ 20-Cookies-Session-Storage/src/persistencia/middlewares/userRol.js
@@ -4,11 +4,15 @@ import session from "express-session";
 export const userPermision =(req, res, next)=>{
     // const user = req.body;
     const statusRol = req.cookies.userInfo;
-    // console.log(req.cookies.userInfo.rol);
-    if(statusRol.rol!== 'admin' || statusRol.rol === 'user' || statusRol === 'undefined' || statusRol === null ){ // admin /o/ user
-        res.status(403).redirect('/users/notAuthorized'); //403: El cliente quiere acceder a un recurso con credenciales de una jerarquía no autorizada.
+    // console.log(statusRol.rol);
+    if(statusRol){
+        if(statusRol.rol!== 'admin' || statusRol.rol === 'user' || statusRol === 'undefined' || statusRol === null ){ // admin /o/ user
+            res.status(403).redirect('/users/notAuthorized'); //403: El cliente quiere acceder a un recurso con credenciales de una jerarquía no autorizada.
+        }else{
+            next(); // puede continuar        
+        } 
     }else{
-        next(); // puede continuar        
+        res.status(403).redirect('/users/notAuthorized')
     }
 }
 
