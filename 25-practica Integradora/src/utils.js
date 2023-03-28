0bf8d9b6058f23bcd0204eedbf604c2f5a93df04
tenconi <47565305+tenconi@users.noreply.github.com>
2023-03-28 08:18:44 +0200

    new start

--- 25-practica Integradora/src/utils.js
+++ 25-practica Integradora/src/utils.js
@@ -4,12 +4,7 @@ import bcrypt from 'bcrypt';
 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// hash password
+// hashPassword
 
-export const hashPassword = async (password) => {
-    return bcrypt.hash(password, 10)
-}
 
-export const comparePassword = async (password, passwordBD) => {
-    return bcrypt.compare(password, passwordBD); // compare: metodo de bcrypt - arroja true/false
-}
+// comparePassword
